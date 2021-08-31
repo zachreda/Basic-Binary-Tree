8ab1a7dc89d21d85203604ecd39b0e6e8992bcc9
Zach Reda <zachreda23@gmail.com>
2021-08-30 21:45:12 -0400

    Initial Commit

---
+++ Basic Binary Tree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Basic Binary Tree
+//
+//  Created by Zach Reda on 8/30/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
